d376762e71c479a5f4908a8ad62ee8c462dc6dd9
Simon Bálint <balint.simon@euroknows.com>
2020-04-09 19:32:33 +0200

    Minor changes in recipies table
    Removed thead.

--- app/views/recipies/recipies.php
+++ app/views/recipies/recipies.php
@@ -5,7 +5,7 @@
     <title>Private Page</title>
     <link rel="stylesheet" type="text/css" href="/PrivatePage/public/stylesheets/main.css">
     <link rel="stylesheet" type="text/css" href="/PrivatePage/public/stylesheets/nav_bar.css">
-    <link rel="stylesheet" type="text/css" href="/PrivatePage/public/stylesheets/studies.css">
+    <link rel="stylesheet" type="text/css" href="/PrivatePage/public/stylesheets/recipies.css">
     <link href='https://fonts.googleapis.com/css?family=Actor' rel='stylesheet'>
     <script src="https://kit.fontawesome.com/399f7f7414.js" crossorigin="anonymous"></script>
 </head>
@@ -19,53 +19,44 @@
         ?>
 
         <main>
-        <div class="title-table">
-                    <h1 class="noselect">Subjects</h1>
+        <div class="wrapper-table">
+            <div class="title-table">
+                    <h1 class="noselect">Recipies</h1>
                     
                     <button id="add_subject" href="add.php"><i class="fas fa-plus"></i>Add Subject</button>
                 </div>
                 
-                <div class="content-table">
-                    <table id="subjects_table" class="studies">
-                                    <thead>
+                <div class="recipies-table">
+                    <table class="recipies">
+                                    <!-- <thead>
                                         <tr>
                                             <th>Name</th>
-                                            <th>Class dates</th>
-                                            <th>Homework deadline</th>
-                                            <th>Next test</th>
-                                            <th>Next exam</th>
-                                            <th>Next assignment</th>
-                                            <th>Current result</th>
+                                            <th>Category</th>
+                                            <th>Time needed</th>
+                                            <th>Link</th>
                                             <th>Actions</th>
                                         </tr>
-                                    </thead>
+                                    </thead> -->
                                     <tbody>
                                         <tr>
-                                            <td>Diszkrét matek</td>
-                                            <td>H 10-12, K 14-16</td>
-                                            <td>-</td>
-                                            <td>2 Days</td>
-                                            <td>?</td>
-                                            <td>-</td>
-                                            <td>-</td>
+                                            <td>Fokhagymás tészta</td>
+                                            <td>Tészta</td>
+                                            <td>10 + 15</td>
+                                            <td><a href="" class="recipie-link"></a>to recipie</td>
                                             <td><a href="edit"></a><i class="fas fa-pencil-alt"></i><a href="delete"></a><i class="fas fa-trash-alt"></i></td>
                                         </tr>
                                         <tr>
-                                            <td>HIA</td>
-                                            <td>Cs 8-10</td>
-                                            <td>-</td>
-                                            <td>4</td>
-                                            <td>-</td>
-                                            <td>May 5th</td>
-                                            <td>43/28 (3)</td>
-                                            <td><a href="edit"></a><i class="fas fa-pencil-alt"></i><a href="delete"></a><i class="fas fa-trash-alt"></i></td>
-                                        </tr>
+                                        <td>Póréhagymás tepsis krumpli</td>
+                                            <td>Krumpli</td>
+                                            <td>20 + 35</td>
+                                            <td><a href="" class="recipie-link"></a>to recipie</td>
+                                            <td><a href="edit"></a><i class="fas fa-pencil-alt"></i><a href="delete"></a><i class="fas fa-trash-alt"></i></td>                                        </tr>
                                     </tbody>
                                 </table>
                 </div>
-        </main>
-
-    </div>
+            </div>
+        </div>
+            </main>
         <?php
             include '../partials/footer.php'
         ?>
